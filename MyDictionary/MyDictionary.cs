68221415f34f66737b6c09d92c0742907c081d25
efecetinalp <efecetinalp@gmail.com>
2022-09-12 22:03:41 +0300

    first commit

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<Tkey, Tvalue>
+    {
+        KeyValuePair<Tkey, Tvalue>[] _items;
+
+        public MyDictionary()
+        {
+            _items = new KeyValuePair<Tkey, Tvalue>[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            KeyValuePair<Tkey, Tvalue>[] tempArray = _items;
+            _items = new KeyValuePair<Tkey, Tvalue>[_items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _items[i] = tempArray[i];
+            }
+
+            _items[_items.Length - 1] = new KeyValuePair<Tkey, Tvalue>(key, value);
+        }
+
+        public void List()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item.Key + " = " + item.Value);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> myDictionary = new MyDictionary<string,int>();
+
+            myDictionary.Add("key1", 12);
+            myDictionary.Add("key2", 0);
+            myDictionary.Add("key3", 9999);
+            myDictionary.Add("key4", 454);
+
+            myDictionary.List();
+        }
+    }
+}
